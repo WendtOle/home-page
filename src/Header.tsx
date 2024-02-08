7ffea9b10609a4c63daace79352a1790df1bb82a
Olee <olee@Oles-Air.fritz.box>
2024-02-08 18:08:55 +0100

    fixup - adjust title letter spacing

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,12 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="flex space-x-2 flex-col xl:flex-row">
-      <h1 className="text-blue-900 text-9xl xl:text-5xl font-mono">
+      <h1 className="text-blue-900 text-9xl xl:text-5xl font-mono tracking-widest">
         Ole Wendt
       </h1>
-      <p className="font-mono text-2xl xl:text-xs">Software Developer</p>
+      <p className="font-mono text-2xl xl:text-xs tracking-tighter">
+        Software Developer
+      </p>
     </div>
   );
 };
