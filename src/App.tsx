0a3f505c10d947aeb2967bf5b904fb9f79c4ea6d
Ole <ole@MBP-von-Ole.fritz.box>
2024-11-10 17:56:55 +0100

    Restyling main layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,21 @@ import { Link } from "./Link";
 export function App() {
   return (
     <div className="flex flex-col h-svh space-between relative dark:bg-gray-600 relative">
-      <div className="flex items-center justify-center h-1/2">
+      <div className="flex flex-col items-center justify-end h-1/4">
         <Header />
       </div>
-      <div className="flex h-1/2 justify-center items-center dark:items-start">
+      <div className="flex h-3/4 justify-center items-center dark:items-start">
         <div className="dark:hidden">
           <Content />
         </div>
-        <div className="flex justify-start items-center p-2 flex-col hidden dark:inline-flex h-full">
-          <img className="rounded-full h-4/5" src={logo} />
-          <Link href="https://soundcloud.com/andreydelany">Soundcloud</Link>
-          <Link href="https://music-production-docs.vercel.app/">
-            Documentation
-          </Link>
+        <div className="flex justify-center items-center p-2 flex-col hidden dark:inline-flex h-full">
+          <img className="rounded-full h-1/2" src={logo} />
+          <div className="flex flex-col items-center gap-4 my-8">
+            <Link href="https://soundcloud.com/andreydelany">Soundcloud</Link>
+            <Link href="https://music-production-docs.vercel.app/">
+              Documentation
+            </Link>
+          </div>
         </div>
       </div>
       <div className="flex justify-end p-2 flex-col items-center xl:items-end">
